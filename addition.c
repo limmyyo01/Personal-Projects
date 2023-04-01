040b609789f53a07ed86bec1470f33637fd31617
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 13:17:35 +0000

    Sat, Apr 1, 2023, 1:17 PM +00:00

--- addition.c
+++ addition.c
@@ -4,6 +4,6 @@ int main (void)
 {
     int firstnum = get_int ("input first number ?");
     int secondnum = get_int ("input second number ?");
-    int result = get_int ( first+ %i )
+    int result = get_int ( firstnum + secondnum )
     printf("%i + %i == result", firstnum, secondnum);
 }
