f174bc2bc7725bb30ab88257e2efa6405ffa1b03
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 12:09:58 +0000

    Sat, Apr 1, 2023, 12:09 PM +00:00

--- great.c
+++ great.c
@@ -6,5 +6,5 @@ int main (void)
     string answer = get_string("what is your name ?");
     printf("hello, %s, have a nice day", answer);
     int answer = get_int ("how old are you ?");
-    printf("hello %s, you are %i, old")
+    printf("hello %s, you are %i, years old", answer)
 }
