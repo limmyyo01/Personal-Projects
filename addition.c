00738cf076904a068310d8278258b7c9f4177067
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 13:05:59 +0000

    Sat, Apr 1, 2023, 1:05 PM +00:00

--- addition.c
+++ addition.c
@@ -4,5 +4,5 @@ int main (void)
 {
     string firstnum = get_string ("input first number ?");
     string secondnum = get_string("input second number ?");
-    printf("%s+,%s" firstnum, secondnum);
+    printf("%s + ,%s" firstnum, secondnum);
 }
