6453470d5e2420348298039548c9d4401ce07de0
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 14:15:08 +0000

    Sat, Apr 1, 2023, 10:15 PM +08:00

--- compare.c
+++ compare.c
@@ -7,9 +7,18 @@ int main (void)
 
 int firstnum = get_int("input first number ?");
 int secondnum = get_int("input second number ?");
-if firstnum > second number{}
+if (firstnum > second number)
+{
+
  printf("first number is greater than second number");
- else if firstnum < secondnum
+ }
+ else if (firstnum < secondnum)
+ {
+
  printf("first number is lesser than second number")
- else ("first number is equal to second number")
+}
+ else
+{
+  ("first number is equal to second number")
+}
 }
