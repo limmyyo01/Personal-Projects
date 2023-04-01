1c033775ede9dafc6b7fd6ea8bee2414538a35ef
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 13:10:13 +0000

    Sat, Apr 1, 2023, 1:10 PM +00:00

--- addition.c
+++ addition.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 int main (void)
 {
-    string firstnum = get_string ("input first number ?");
-    string secondnum = get_string("input second number ?");
+    int firstnum = get_int ("input first number ?");
+    int secondnum = get_int ("input second number ?");
     printf("%i + ,%i", firstnum, secondnum);
 }
