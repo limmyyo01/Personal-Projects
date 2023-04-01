8f1ad70b772af098f093813d9a46c5c55350a0ae
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 14:08:24 +0000

    Sat, Apr 1, 2023, 10:08 PM +08:00

---
+++ compare.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+main (void)
+{
+
+
+int firstnum = get_int("input first number ?");
+int secondnum = get_int("input second number ?");
+if firstnum > second number
+ printf("first number is greater than second number)
+ else
+}
