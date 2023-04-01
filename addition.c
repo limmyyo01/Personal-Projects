36f4a7bc07c5540d39903a39f2250e8dfbad1376
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-04-01 13:28:56 +0000

    Sat, Apr 1, 2023, 1:28 PM +00:00

--- addition.c
+++ addition.c
@@ -4,6 +4,6 @@ int main (void)
 {
     int firstnum = get_int ("input first number ?");
     int secondnum = get_int ("input second number ?");
-    int result = firstnum + secondnum * 2 ;
+    int result = (firstnum + secondnum) * 2 ;
     printf("%i + %i * 2 = %i", firstnum, secondnum, result);
 }
