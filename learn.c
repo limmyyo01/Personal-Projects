1d81e0b3d05c5b649af5ae6ca26acd8e84ee9c95
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-06-07 10:01:36 +0000

    Wed, Jun 7, 2023, 11:01 AM +01:00

--- learn.c
+++ learn.c
@@ -2,8 +2,8 @@
 #include <cs50.h>
 int main (void)
 {
-    string first = get_string ( "what is your first name ?");
+    string first = get_string ("what is your first name?");
     string second = get_string ("what is your second name ?");
-    printf("hello, %s %s\n" first second ");
+    printf("hello, %s %s\n", first, second ");
 
 }
