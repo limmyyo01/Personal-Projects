5317f97290919789ee30dc3f5ff03c020193db42
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-12-21 13:05:42 +0000

    Thu, Dec 21, 2023, 1:05 PM +00:00

--- calculator.c
+++ calculator.c
@@ -6,20 +6,20 @@ int main(void)
  long firstNumber =get_long("first number? ");
  long secondNumber =get_long("second number? ");
  char symbol =get_char("symbol? ");
-    if(symbol == + )
+    if(symbol == '+' )
     {
        int sum = firstNumber + secondNumber;
         printf("%d\n", sum);
     }
-    else if (symbol == -)
+    else if (symbol == '-')
     {
-        int min = firsstNumber - secondNumber;
+        int min = firstNumber - secondNumber;
         printf("%d\n", min);
-    }else if (symbol == * )
+    }else if (symbol == '*')
     {
         int times = firstNumber*secondNumber;
         printf("%d\n", times);
-    }else if (symbol == /)
+    }else if (symbol == '/')
     {
         int div = firstNumber/secondNumber;
         printf("%d\n", div);
