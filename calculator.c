6bf563166013d4c223a2992fbe81ceb8f83d9628
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2023-12-21 12:48:47 +0000

    Thu, Dec 21, 2023, 12:48 PM +00:00

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@ int main(void)
  long firstNumber =get_long("first number? ");
  long secondNumber =get_long("second number? ");
  char symbol =get_char("symbol? ");
-    if(symbol == '+')
+    if(symbol == (+ - * /)
     {
         int sum = firstNumber + secondNumber;
         printf("%d\n", sum);
