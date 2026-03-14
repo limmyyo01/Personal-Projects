1f2f60936b420ac441eac21a750a4247f6da6fa6
limmyyo01 <129593470+limmyyo01@users.noreply.github.com>
2026-03-14 15:47:39 +0000

    Delete learn.c

--- learn.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-int main (void)
-{
-    string first = get_string ("what is your first name? ");
-    string second = get_string ("what is your second name? ");
-    printf("hello, %s %s\n", first, second );
-
-}
